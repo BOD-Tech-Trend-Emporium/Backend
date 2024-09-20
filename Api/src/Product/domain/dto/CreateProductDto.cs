08ad7e45b1ec72f79e9f16c8d05747fe9c288579
Santiago Alvarez Ricardo <Santiago.Alvarez@endava.com>
2024-09-19 21:21:46 -0500

    add mapper in products from createDTO to Entities

--- Api/src/Product/domain/dto/CreateProductDto.cs
+++ Api/src/Product/domain/dto/CreateProductDto.cs
@@ -1,12 +1,13 @@
-﻿namespace Api.src.Product.domain.dto
+﻿
+namespace Api.src.Product.domain.dto
 {
     public class CreateProductDto
     {
-        public string Title { get; set; }
+        public required string Title { get; set; }
         public float Price { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
-        public string Image { get; set; }
+        public required string Description { get; set; }
+        public required string Category { get; set; }
+        public required string Image { get; set; }
         public int Stock { get; set; }
     }
 }
