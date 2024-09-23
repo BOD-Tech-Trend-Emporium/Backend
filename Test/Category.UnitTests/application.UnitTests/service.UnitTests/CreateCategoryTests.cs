2e95e24e8f6d2384b94d3b30d30fb6cb11cac32f
Jhoan Oswaldo Ome Vega <Jhoan-Oswaldo.Ome@endava.com>
2024-09-23 15:50:33 -0500

    Add unit test to create and view categories

--- Test/Category.UnitTests/application.UnitTests/service.UnitTests/CreateCategoryTests.cs
+++ Test/Category.UnitTests/application.UnitTests/service.UnitTests/CreateCategoryTests.cs
@@ -1,5 +1,6 @@
 ﻿using Api.src.Category.application.service;
 using Api.src.Category.domain.entity;
+using Api.src.Category.domain.enums;
 using Api.src.Common.exceptions;
 using backend.Data;
 using backend.src.User.domain.enums;
@@ -23,7 +24,7 @@ namespace Test.Category.UnitTests.application.UnitTests.service.UnitTests
         
 
         [Fact]
-        public async void CreateCategory_Run_ReturnCategory() {
+        public async void Given_NewCategory_When_CategoryAlreadyCreated_Then_ConflictException() {
             //Arrange
             var dbContext = await GetDataBaseContext();
             CategoryEntity category = new() { Name = "Books", Status = Api.src.Category.domain.enums.CategoryStatus.Created};
@@ -34,12 +35,48 @@ namespace Test.Category.UnitTests.application.UnitTests.service.UnitTests
             CreateCategory createCategory = new CreateCategory(dbContext);
 
             //ACT
-            //var result = await createCategory.Run(category, role);
             Func<Task> act = () => createCategory.Run(category, role);
            
             //Assert
             await act.Should().ThrowAsync<ConflictException>()
             .WithMessage("Category already exists");
+
+        }
+
+        [Fact]
+        public async void Given_AdminCreatesANewCategory_When_CategoryDoesNotExist_Then_CategoryEntity()
+        {
+            //Arrange
+            var dbContext = await GetDataBaseContext();
+            CategoryEntity category = new() { Name = "Books", Status = CategoryStatus.Created };
+            UserRole role = UserRole.Admin;
+            CategoryStatus categoryStatus = CategoryStatus.Created;
+            CreateCategory createCategory = new CreateCategory(dbContext);
+
+            //ACT
+            CategoryEntity result = await createCategory.Run(category, role);
+
+            //Assert
+            result.Should().Be(category);
+            result.Status.Should().Be(categoryStatus);
+
+        }
+        [Fact]
+        public async void Given_EmployeeCreatesANewCategory_When_CategoryDoesNotExist_Then_CategoryEntity()
+        {
+            //Arrange
+            var dbContext = await GetDataBaseContext();
+            CategoryEntity category = new() { Name = "Books", Status = CategoryStatus.Created };
+            UserRole role = UserRole.Employee;
+            CategoryStatus categoryStatus = CategoryStatus.ToCreate;
+            CreateCategory createCategory = new CreateCategory(dbContext);
+
+            //ACT
+            CategoryEntity result = await createCategory.Run(category, role);
+
+            //Assert
+            result.Should().Be(category);
+            result.Status.Should().Be(categoryStatus);
 
         }
     }
