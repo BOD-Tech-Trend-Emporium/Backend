6fa77957af88f9de6da682317727cac14888d631
Jhoan Oswaldo Ome Vega <Jhoan-Oswaldo.Ome@endava.com>
2024-09-24 18:48:51 -0500

    Add endpoint to delete categories

--- Api/src/Category/infraestructure/api/CategoryService.cs
+++ Api/src/Category/infraestructure/api/CategoryService.cs
@@ -1,4 +1,5 @@
 ﻿using Api.src.Category.application.service;
+using Api.src.Category.domain.dto;
 using Api.src.Category.domain.entity;
 using Api.src.Category.domain.repository;
 using backend.Data;
@@ -8,22 +9,29 @@ namespace Api.src.Category.infraestructure.api
 {
     public class CategoryService : CategoryRepository
     {
-        private GetAllApprovedCategories getAllApprovedCategories;
-        private CreateCategory createCategory;
+        private GetAllApprovedCategories _getAllApprovedCategories;
+        private CreateCategory _createCategory;
+        private DeleteCategoryById _deleteCategory;
         public CategoryService(ApplicationDBContext context)
         {
-            getAllApprovedCategories = new GetAllApprovedCategories(context);
-            createCategory = new CreateCategory(context);
+            _getAllApprovedCategories = new GetAllApprovedCategories(context);
+            _createCategory = new CreateCategory(context);
+            _deleteCategory = new DeleteCategoryById(context);
         }
 
         public async Task<CategoryEntity> CreateAsync(CategoryEntity category, UserRole role)
         {
-            return await createCategory.Run(category, role);
+            return await _createCategory.Run(category, role);
+        }
+
+        public async Task<DeleteCategoryByIdResponseDto> DeleteCategoryByIdAsync(Guid id, UserRole role)
+        {
+            return await _deleteCategory.Run(id, role);
         }
 
         public async Task<List<CategoryEntity>> GetAllApprovedAsync()
         {
-            return await getAllApprovedCategories.Run();
+            return await _getAllApprovedCategories.Run();
         }
 
     }
