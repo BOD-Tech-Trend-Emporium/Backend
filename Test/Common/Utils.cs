a8700d83de1add03d2272197c510b7c4f0dfbcb2
Santiago Alvarez Ricardo <Santiago.Alvarez@endava.com>
2024-09-24 10:55:13 -0500

    add basic test to create product

---
+++ Test/Common/Utils.cs
@@ -0,0 +1,23 @@
+﻿using backend.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Common
+{
+    public class Utils
+    {
+        public static ApplicationDBContext GetDataBaseContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDBContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var databaseContext = new ApplicationDBContext(options);
+            databaseContext.Database.EnsureCreated();
+            return databaseContext;
+        }
+    }
+}
