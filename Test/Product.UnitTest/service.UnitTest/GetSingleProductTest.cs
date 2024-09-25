15df8e97e64e1417001791458171704ef9e1fbfb
Santiago Alvarez Ricardo <Santiago.Alvarez@endava.com>
2024-09-24 23:13:18 -0500

    finishing test to get product by id (error flows)

--- Test/Product.UnitTest/service.UnitTest/GetSingleProductTest.cs
+++ Test/Product.UnitTest/service.UnitTest/GetSingleProductTest.cs
@@ -1,5 +1,6 @@
 ﻿using Api.src.Category.domain.entity;
 using Api.src.Category.domain.enums;
+using Api.src.Common.exceptions;
 using Api.src.Product.application.service;
 using Api.src.Product.domain.dto;
 using Api.src.Product.domain.entity;
@@ -59,6 +60,68 @@ namespace Test.Product.UnitTest.service.UnitTest
             act.Image.Should().Be(image);
             act.Title.Should().Be(title);
             act.Category.Should().Be(categoryName);
+        }
+
+        [Fact]
+        public async void Given_GetsingleProduct_When_ProductNOTExists_Then_ThrowNotFoundException()
+        {
+            // Create new Category
+            var dbContext = Utils.GetDataBaseContext();
+
+            var nonExistingProduct = Guid.NewGuid();
+            
+            GetProductById getAllProducts = new(dbContext);
+
+            // ACT
+            Func<Task> act = ()=> getAllProducts.Run(nonExistingProduct);
+
+            // Assert
+            await act.Should().ThrowAsync<NotFoundException>().WithMessage($"product with id {nonExistingProduct} not found");
+        }
+
+        [Fact]
+        public async void Given_GetsingleProduct_When_ProductNOTAvailable_Then_ThrowNotFoundException()
+        {
+            // Create new Category
+            var dbContext = Utils.GetDataBaseContext();
+
+            Guid categoryId = Guid.NewGuid();
+            var categoryName = "Books";
+            CategoryEntity category = new()
+            {
+                Id = categoryId,
+                Name = categoryName,
+                Status = CategoryStatus.Created
+            };
+            dbContext.Category.Add(category);
+
+            // Create products with existing category
+            var description = "Description";
+            var image = "image";
+            var title = "Title";
+            var stock = 30;
+            var prId = Guid.NewGuid();
+            ProductEntity product = new()
+            {
+                Id = prId,
+                Image = image,
+                Title = title,
+                Category = category,
+                Stock = stock,
+                Description = description,
+                Status = Api.src.Product.domain.enums.ProductStatus.Removed,
+            };
+            dbContext.Product.Add(product);
+
+            await dbContext.SaveChangesAsync();
+
+            GetProductById getAllProducts = new(dbContext);
+
+            // ACT
+            Func<Task> act = () => getAllProducts.Run(prId);
+
+            // Assert
+            await act.Should().ThrowAsync<NotFoundException>().WithMessage($"product with id {prId} not found");
         }
     }
 }
