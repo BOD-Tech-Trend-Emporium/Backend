d5f7848563ee3e7fc9ec86163eff2381071b66af
Jhoan Oswaldo Ome Vega <Jhoan-Oswaldo.Ome@endava.com>
2024-09-28 19:57:45 -0500

    Add test to add products in the cart and buy them

---
+++ Test/src/Cart.UnitTests/service.UnitTest/CreateCartTests.cs
@@ -0,0 +1,80 @@
+﻿using Api.src.Cart.application.service;
+using Api.src.Cart.domain.dto;
+using Api.src.Cart.domain.entity;
+using Api.src.Cart.domain.enums;
+using Api.src.Category.domain.enums;
+using Api.src.Common.exceptions;
+using Api.src.User.domain.enums;
+using backend.Data;
+using backend.src.User.domain.entity;
+using backend.src.User.domain.enums;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace Test.src.Cart.UnitTests.service.UnitTest
+{
+    public class CreateCartTests
+    {
+        private async Task<ApplicationDBContext> GetDataBaseContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDBContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var databaseContext = new ApplicationDBContext(options);
+            databaseContext.Database.EnsureCreated();
+            return databaseContext;
+        }
+
+        [Fact]
+        public async void Given_ANewCart_When_UserHasOneWithPendingStatus_Then_ConflictException()
+        {
+            //Arrange
+            var dbContext = await GetDataBaseContext();
+            UserEntity user = new() { Name = "Sir Isaac Netwon", Email = "Isaac@gmail.com", UserName = "Isaac21", Password = "contrasena", Role = UserRole.Shopper, Status = UserStatus.Created };
+            CartEntity cart = new() { User = user, State = CartState.Pending};
+            dbContext.User.Add(user);
+            dbContext.Cart.Add(cart);
+            await dbContext.SaveChangesAsync();
+
+             CreateCart createCart = new CreateCart(dbContext);
+
+            //ACT
+            Func<Task> act = () => createCart.Run(user.Id);
+
+            //Assert
+            await act.Should().ThrowAsync<ConflictException>()
+            .WithMessage("The cart already exists");
+
+        }
+
+        [Fact]
+        public async void Given_ANewCart_When_UserDoesNotHaveOneOrApprovedStatus_Then_CartResponse()
+        {
+            //Arrange
+            var dbContext = await GetDataBaseContext();
+            UserEntity user = new() { Name = "Sir Isaac Netwon", Email = "Isaac@gmail.com", UserName = "Isaac21", Password = "contrasena", Role = UserRole.Shopper, Status = UserStatus.Created };
+            CartEntity cart = new() { User = user, State = CartState.Approved };
+            dbContext.User.Add(user);
+            dbContext.Cart.Add(cart);
+            await dbContext.SaveChangesAsync();
+            CartResponse cartResponse = new CartResponse() { UserId = user.Id, ShoppingCart = new List<Guid>(), ShippingCost = 0, FinalTotal = 0 };
+
+            CreateCart createCart = new CreateCart(dbContext);
+
+            //ACT
+            var result = await createCart.Run(user.Id);
+
+            //Assert
+            result.UserId.Should().Be(user.Id);
+            result.ShoppingCart.Should().BeEmpty();
+            result.Should().NotBeNull();
+            result.FinalTotal.Should().Be(cartResponse.FinalTotal);
+
+        }
+
+    }
+
+
+}
---
+++ Test/src/Cart.UnitTests/service.UnitTest/CreatePurchaseTests.cs
@@ -0,0 +1,116 @@
+﻿using Api.src.Cart.application.service;
+using Api.src.Cart.domain.dto;
+using Api.src.Cart.domain.entity;
+using Api.src.Cart.domain.enums;
+using Api.src.CartToProduct.domain.entity;
+using Api.src.Category.domain.entity;
+using Api.src.Category.domain.enums;
+using Api.src.Common.exceptions;
+using Api.src.Price.domain.entity;
+using Api.src.Product.domain.entity;
+using Api.src.Product.domain.enums;
+using Api.src.User.domain.enums;
+using backend.Data;
+using backend.src.User.domain.entity;
+using backend.src.User.domain.enums;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.src.Cart.UnitTests.service.UnitTest
+{
+    public class CreatePurchaseTests
+    {
+        private async Task<ApplicationDBContext> GetDataBaseContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDBContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var databaseContext = new ApplicationDBContext(options);
+            databaseContext.Database.EnsureCreated();
+            return databaseContext;
+        }
+
+        [Fact]
+        public async void Given_APurchase_When_TheCartDoesNotExist_Then_NotFoundException()
+        {
+            //Arrange
+            var dbContext = await GetDataBaseContext();
+            UserEntity user = new() { Name = "Sir Isaac Netwon", Email = "Isaac@gmail.com", UserName = "Isaac21", Password = "contrasena", Role = UserRole.Shopper, Status = UserStatus.Created };
+            dbContext.User.Add(user);
+            await dbContext.SaveChangesAsync();
+
+            CreatePurchase createPurchase = new CreatePurchase(dbContext);
+
+            //ACT
+            Func<Task> act = () => createPurchase.Run(user.Id);
+
+            //Assert
+            await act.Should().ThrowAsync<NotFoundException>()
+            .WithMessage("Cart does not exist");
+
+        }
+
+        [Fact]
+        public async void Given_APurchase_When_InsufficientProductInInventory_Then_ConflictException()
+        {
+            //Arrange
+            var dbContext = await GetDataBaseContext();
+            UserEntity user = new() { Name = "Sir Isaac Netwon", Email = "Isaac@gmail.com", UserName = "Isaac21", Password = "contrasena", Role = UserRole.Shopper, Status = UserStatus.Created };
+            CartEntity cart = new() { User = user, State = CartState.Pending };
+            CategoryEntity categoryEntity = new CategoryEntity() { Name = "Music", Status = CategoryStatus.Created};
+            ProductEntity product = new ProductEntity() { Category = categoryEntity, Title = "Pink Floyd album",Description="The best one",Image="www.pinkfloyd.com" ,Stock = 0,Status= ProductStatus.Created };
+            PriceEntity priceEntity = new PriceEntity() { Product=product,Current = true, Price = 120 };
+            CartToProductEntity cartToProduct = new CartToProductEntity() {Cart=cart, Price=priceEntity,Quantity=10 };
+            dbContext.User.Add(user);
+            dbContext.Cart.Add(cart);
+            dbContext.Category.Add(categoryEntity);
+            dbContext.Price.Add(priceEntity);
+            dbContext.CartToProduct.Add(cartToProduct);
+            dbContext.Product.Add(product);
+
+            await dbContext.SaveChangesAsync();
+
+            CreatePurchase createPurchase = new CreatePurchase(dbContext);
+
+            //ACT
+            Func<Task> act = () => createPurchase.Run(user.Id);
+
+            //Assert
+            await act.Should().ThrowAsync<ConflictException>()
+            .WithMessage("Insufficient product in inventory");
+
+        }
+        [Fact]
+        public async void Given_APurchase_When_TheProductIsEnough_Then_PurchaseResponse()
+        {
+            //Arrange
+
+            var dbContext = await GetDataBaseContext();
+            UserEntity user = new() { Name = "Sir Isaac Netwon", Email = "Isaac@gmail.com", UserName = "Isaac21", Password = "contrasena", Role = UserRole.Shopper, Status = UserStatus.Created };
+            CartEntity cart = new() { User = user, State = CartState.Pending };
+            CategoryEntity categoryEntity = new CategoryEntity() { Name = "Music", Status = CategoryStatus.Created };
+            ProductEntity product = new ProductEntity() { Category = categoryEntity, Title = "Pink Floyd album", Description = "The best one", Image = "www.pinkfloyd.com", Stock = 20, Status = ProductStatus.Created };
+            PriceEntity priceEntity = new PriceEntity() { Product = product, Current = true, Price = 120 };
+            CartToProductEntity cartToProduct = new CartToProductEntity() { Cart = cart, Price = priceEntity, Quantity = 10 };
+            dbContext.User.Add(user);
+            dbContext.Cart.Add(cart);
+            dbContext.Category.Add(categoryEntity);
+            dbContext.Price.Add(priceEntity);
+            dbContext.CartToProduct.Add(cartToProduct);
+            dbContext.Product.Add(product);
+            await dbContext.SaveChangesAsync();
+            var expected = new PurchaseResponse() { Message = "Successful purchase" };
+            //ACT
+            CreatePurchase createPurchase = new CreatePurchase(dbContext);
+            var result =await createPurchase.Run(user.Id);
+            //Assert
+            result.Message.Should().Be(expected.Message);
+
+        }
+    }
+}
