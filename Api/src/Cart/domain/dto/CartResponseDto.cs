5e21c0d96459f9fd8c40df25a755ebe5d39979a4
Jhoan Oswaldo Ome Vega <Jhoan-Oswaldo.Ome@endava.com>
2024-09-30 12:17:45 -0500

    Change the names of DTOs

---
+++ Api/src/Cart/domain/dto/CartResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.src.Cart.domain.dto
+{
+    public class CartResponseDto
+    {
+        public Guid UserId { get; set; }
+        public List<Guid> ShoppingCart { get; set; }
+        public float ShippingCost { get; set; }
+        public float FinalTotal { get; set; }
+    }
+}
---
+++ Api/src/Cart/domain/dto/PurchaseResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Api.src.Cart.domain.dto
+{
+    public class PurchaseResponseDto
+    {
+        public string Message { get; set; }
+    }
+}
